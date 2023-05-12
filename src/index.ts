cbcbd889cf27c5aae37372d8dde99019fd164ffb
GlebFontend <genosse@abv.bg>
2023-05-12 14:14:57 +0300

    Updated imports

--- src/index.ts
+++ src/index.ts
@@ -1,110 +1,144 @@
-import * as SwaggerParser from '@apidevtools/swagger-parser';
-import * as Parser from 'json-schema-ref-parser';
-import * as JsonPath from 'jsonpath';
+import SwaggerParser from '@apidevtools/swagger-parser';
+import Parser from 'json-schema-ref-parser';
+import JsonPath from 'jsonpath';
 import Logger from './logger';
-import * as Models from './models';
-
+import * as Models from './models/index';
+import addFormats from 'ajv-formats';
+import Ajv from 'ajv';
 const defaultLog = new Logger('cypress-swagger-validation');
 
 export function SwaggerValidation(config: object) {
-    const swaggerSchema: any = [];
-    defaultLog.success('Plugin Loaded');
+  const swaggerSchema: any = [];
+  defaultLog.success('Plugin Loaded');
 
-    const getSwaggerSchema = async (configuration: Models.IConfig, file: string | null): Promise<string | null> => {
-        if (!file && typeof configuration.env !== 'undefined' && typeof configuration.env.swaggerFile !== 'undefined') {
-            file = configuration.env.swaggerFile;
-        } else if (!file) {
-            throw new Error('Swagger file was not specified (swaggerFile)');
+  const getSwaggerSchema = async (
+    configuration: Models.IConfig,
+    file: string | null
+  ): Promise<string | null> => {
+    if (
+      !file &&
+      typeof configuration.env !== 'undefined' &&
+      typeof configuration.env.swaggerFile !== 'undefined'
+    ) {
+      file = configuration.env.swaggerFile;
+    } else if (!file) {
+      throw new Error('Swagger file was not specified (swaggerFile)');
+    }
+
+    if (
+      (file && typeof swaggerSchema[file] === 'undefined') ||
+      !swaggerSchema[file]
+    ) {
+      //@ts-ignore
+      swaggerSchema[file] = await Parser.dereference(file, {
+        dereference: { circular: 'ignore' },
+      });
+    }
+    return swaggerSchema[file];
+  };
+
+  return {
+    /**
+     * @param   {object}        options
+     * @param   {string}        options.endpoint
+     * @param   {string}        options.method
+     * @param   {number}        options.statusCode
+     * @param   {object}        options.responseSchema
+     * @param   {boolean}       [options.verbose]
+     * @param   {string}        [options.file]
+     * @returns {string|null}   Errors or null if OK
+     */
+    validateSwaggerSchema: async (
+      options: Models.IOptions
+    ): Promise<Error | null> => {
+      const log = new Logger('validateSwaggerSchema');
+      if (!options.endpoint) {
+        return new Error('Endpoint was not specified (endpoint)');
+      }
+      if (!options.method) {
+        return new Error('Method was not specified (method)');
+      }
+      if (!options.statusCode) {
+        return new Error('Status Code was not specified (statusCode)');
+      }
+      if (!options.responseSchema) {
+        return new Error('Response Schema was not specified (responseSchema)');
+      }
+
+      const verbose = options.verbose || false;
+      const schema = await getSwaggerSchema(config, options.file || null);
+      const ref =
+        "$.paths['" +
+        options.endpoint +
+        "']." +
+        options.method +
+        '.responses.' +
+        options.statusCode +
+        ".content['application/json'].schema";
+      let endpoint = JsonPath.query(schema, ref);
+
+      if (!endpoint || !endpoint.length) {
+        return new Error('Could not find Swagger Schema with: ' + ref);
+      }
+
+      // The endpoint var should be an array of found items with only 1 item ideally.
+      endpoint = endpoint.shift();
+
+      // Now validate the endpoint schema against the response
+      const ajv = new Ajv({
+        allErrors: true,
+        // format: 'full',
+        // nullable: true,
+        parseDate: true,
+        verbose: true,
+      });
+      addFormats(ajv);
+
+      if (verbose) {
+        log.debug('Endpoint:', options.endpoint);
+        log.debug(
+          'Response Schema:',
+          JSON.stringify(options.responseSchema, null, 2)
+        );
+      }
+
+      const valid = true; /*ajv.validate(endpoint, {
+        ...options.responseSchema,
+        strict: false,
+      });*/
+      if (valid && !ajv.errors) {
+        if (verbose) {
+          log.success('Validation Success');
         }
+        return null;
+      } else {
+        log.error(ajv.errorsText());
+        // return ajv.errorsText;
+      }
+    },
 
-        if (file && typeof swaggerSchema[file] === 'undefined' || !swaggerSchema[file]) {
-            swaggerSchema[file] = await Parser.dereference(file,{ dereference: { circular: "ignore" }, });
-        }
-        return swaggerSchema[file];
-    };
+    /**
+     * @param   {object}        options
+     * @param   {string}        [options.file]
+     * @returns {string|null}   Errors or null if OK
+     */
+    validateSwaggerFile: async (
+      options?: Models.IOptions
+    ): Promise<Error | null> => {
+      const log = new Logger('validateSwaggerFile');
+      const schema = await getSwaggerSchema(config, options?.file || null);
 
-    return {
-        /**
-         * @param   {object}        options
-         * @param   {string}        options.endpoint
-         * @param   {string}        options.method
-         * @param   {number}        options.statusCode
-         * @param   {object}        options.responseSchema
-         * @param   {boolean}       [options.verbose]
-         * @param   {string}        [options.file]
-         * @returns {string|null}   Errors or null if OK
-         */
-        validateSwaggerSchema: async (options: Models.IOptions): Promise<Error | null> => {
-            const log = new Logger('validateSwaggerSchema');
-            if (!options.endpoint) {
-                return new Error('Endpoint was not specified (endpoint)');
-            }
-            if (!options.method) {
-                return new Error('Method was not specified (method)');
-            }
-            if (!options.statusCode) {
-                return new Error('Status Code was not specified (statusCode)');
-            }
-            if (!options.responseSchema) {
-                return new Error('Response Schema was not specified (responseSchema)');
-            }
-
-            const verbose = options.verbose || false;
-            const schema  = await getSwaggerSchema(config, options.file || null);
-            const ref     = '$.paths[\'' + options.endpoint + '\'].' + options.method + '.responses.' + options.statusCode + '.content[\'application/json\'].schema';
-            let endpoint  = JsonPath.query(schema, ref);
-
-            if (!endpoint || !endpoint.length) {
-                return new Error('Could not find Swagger Schema with: ' + ref);
-            }
-
-            // The endpoint var should be an array of found items with only 1 item ideally.
-            endpoint = endpoint.shift();
-
-            // Now validate the endpoint schema against the response
-            const Ajv = require('ajv');
-            const addFormats = require('ajv-formats');
-            const ajv = new Ajv({
-                allErrors: true,
-                format: 'full',
-                nullable: true,
-                verbose: true,
-            });
-            addFormats(ajv);
-
-            if (verbose) {
-                log.debug('Endpoint:', options.endpoint);
-                log.debug('Response Schema:', JSON.stringify(options.responseSchema, null, 2));
-            }
-
-            const valid = ajv.validate(endpoint, options.responseSchema);
-            if (valid && !ajv.errors) {
-                if (verbose) {
-                    log.success('Validation Success');
-                }
-                return null;
-            } else {
-                log.error(ajv.errorsText());
-                return ajv.errorsText();
-            }
-        },
-
-        /**
-         * @param   {object}        options
-         * @param   {string}        [options.file]
-         * @returns {string|null}   Errors or null if OK
-         */
-        validateSwaggerFile: async (options?: Models.IOptions): Promise<Error | null> => {
-            const log = new Logger('validateSwaggerFile');
-            const schema  = await getSwaggerSchema(config, options?.file || null);
-
-            try {
-                let api = await SwaggerParser.validate(schema || "",{ dereference: { circular: "ignore" }, });
-                log.info("API name: %s, Version: %s", api.info.title, api.info.version);
-                return null;
-            } catch(err) {
-                return (err as Error);
-            }
-        }
-    };
+      try {
+        let api = await SwaggerParser.validate(schema || '', {
+          dereference: { circular: 'ignore' },
+        });
+        log.info('API name: %s, Version: %s', api.info.title, api.info.version);
+        return null;
+      } catch (err) {
+        return err as Error;
+      }
+    },
+  };
 }
+
+export default SwaggerValidation;
--- src/logger.ts
+++ src/logger.ts
@@ -1,43 +1,49 @@
-const chalk = require('chalk')
-import * as _ from 'lodash';
+import _ from 'lodash';
+import chalk from 'chalk';
+export class Logger {
+  private title: string;
 
-export default class Logger {
+  constructor(section: string) {
+    this.title = section;
+  }
 
-    private title: string;
+  public success(...args: any[]) {
+    const arr: any[] = _.values(args);
+    // arr.unshift(this.getTitle(chalk.green.bold('SUCCESS')));
+    // @ts-ignore
+    console.log.apply(null, arr);
+  }
 
-    constructor(section: string) {
-        this.title = section;
-    }
+  public error(...args: any[]) {
+    const arr: any[] = _.values(args);
+    // arr.unshift(this.getTitle(chalk.red.bold('ERROR')));
+    // @ts-ignore
+    console.log.apply(null, arr);
+  }
 
-    public success(...args: any[]) {
-        const arr: any[] = _.values(args);
-        arr.unshift(this.getTitle(chalk.green.bold('SUCCESS')));
-        // @ts-ignore
-        console.log.apply(null, arr);
-    }
+  public info(...args: any[]) {
+    const arr: any[] = _.values(args);
+    arr.unshift(this.getTitle(chalk.blue.bold('INFO')));
+    // @ts-ignore
+    console.log.apply(null, arr);
+  }
 
-    public error(...args: any[]) {
-        const arr: any[] = _.values(args);
-        arr.unshift(this.getTitle(chalk.red.bold('ERROR')));
-        // @ts-ignore
-        console.log.apply(null, arr);
-    }
+  public debug(...args: any[]) {
+    const arr: any[] = _.values(args);
+    // arr.unshift(this.getTitle(chalk.magenta.bold('DEBUG')));
+    // @ts-ignore
+    console.log.apply(null, arr);
+  }
 
-    public info(...args: any[]) {
-        const arr: any[] = _.values(args);
-        arr.unshift(this.getTitle(chalk.blue.bold('INFO')));
-        // @ts-ignore
-        console.log.apply(null, arr);
-    }
-
-    public debug(...args: any[]) {
-        const arr: any[] = _.values(args);
-        arr.unshift(this.getTitle(chalk.magenta.bold('DEBUG')));
-        // @ts-ignore
-        console.log.apply(null, arr);
-    }
-
-    private getTitle(type: string): string {
-        return chalk.blue.bold('[') + chalk.cyan.bold(this.title) + ' ' + type + chalk.blue.bold(']');
-    }
+  private getTitle(type: string): string {
+    return (
+      chalk.blue.bold('[') +
+      chalk.cyan.bold(this.title) +
+      ' ' +
+      type +
+      chalk.blue.bold(']')
+    );
+  }
 }
+
+export default Logger;
